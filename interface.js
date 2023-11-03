018b7e0b59932c72dc78b781d93489ae028d6718
Diyorbek <dgwww1122@gmail.com>
2023-11-03 15:27:44 +0500

    dsfds

--- interface.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// type TypeOf = number | string;
-function CarInterface(d) {
-    console.log(d);
-}
-exports.default = CarInterface;
